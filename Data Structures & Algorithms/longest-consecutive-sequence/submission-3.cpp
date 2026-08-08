b4caf39660530453172ca53a975005cee5bfde3d
Nikhil Ratta <61683657+NikhilRattaNITKKR@users.noreply.github.com>
2026-08-08 18:22:47 +0530

    Add: longest-consecutive-sequence - submission-3

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-3.cpp
@@ -0,0 +1,26 @@
+class Solution {
+   public:
+    int longestConsecutive(vector<int>& nums) {
+        if (nums.size() == 0) return 0;
+        unordered_map<int, int> ht;
+
+        for (auto& num : nums) {
+            ht[num]++;
+        }
+
+        int len = 1, maxi = 0;
+        for (auto& entry : ht) {
+            int key = entry.first;
+            if (ht.count(key - 1) == 0 && ht.count(key + 1) > 0) {
+                len = 1;
+                key++;
+                while (ht.count(key)) {
+                    len++;
+                    key++;
+                }
+            }
+            maxi = max(maxi, len);
+        }
+        return maxi;
+    }
+};
